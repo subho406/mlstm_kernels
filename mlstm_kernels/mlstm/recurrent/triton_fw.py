7b9c6930fc1a0968ebb5bf497438fa2b352fe9b5
Maximilian Beck <beck@ml.jku.at>
2024-08-10 14:45:02 +0200

    recurrent step matches for a single block.

--- mlstm_kernels/mlstm/recurrent/triton_fw.py
+++ mlstm_kernels/mlstm/recurrent/triton_fw.py
@@ -20,7 +20,7 @@ We want to compare this to the torch implementation in mlstm_kernels/mlstm/recur
 
 @triton.jit
 def _recurrent_step_fw_kernel(
-    matC_old,  # (B, NH, DHQK, DHV)
+    matC_old_val,  # (B, NH, DHQK, DHV)
     vecN_old,  # (B, NH, DHQK)
     scaM_old,  # (B, NH, 1)
     vecQ,  # (B, NH, DHQK)
@@ -32,17 +32,24 @@ def _recurrent_step_fw_kernel(
     matC_new,  # (B, NH, DHQK, DHV)
     vecN_new,  # (B, NH, DHQK)
     scaM_new,  # (B, NH, 1)
-    s_matC_b, 
+    qk_scale,
+    s_matC_b,
     s_matC_nh,
     s_matC_dhqk,
+    s_matC_dhv,
     s_vecN_b,
     s_vecN_nh,
+    s_vecN_dhqk,
     s_scaM_b,
     s_scaM_nh,
     s_vecQK_b,
     s_vecQK_nh,
+    s_vecQK_dhqk,
     s_vecVH_b,
     s_vecVH_nh,
+    s_vecVH_dhv,
+    s_scaIF_b,
+    s_scaIF_nh,
     B,
     NH,
     DHQK: tl.constexpr,
@@ -51,7 +58,122 @@ def _recurrent_step_fw_kernel(
     BLOCK_DV: tl.constexpr,  # DHV = BLOCK_DV * NUM_BLOCKS_DV
     EPS: tl.constexpr = 1e-6,
 ):
-    pass
+    i_dhqk, i_dhv, i_bnh = tl.program_id(0), tl.program_id(1), tl.program_id(2)
+
+    #? Define pointers
+    matC_old_bptr = tl.make_block_ptr(
+        base=matC_old_val + i_bnh * s_matC_nh,
+        shape=(BLOCK_DQK, BLOCK_DV),
+        strides=(s_matC_dhqk, s_matC_dhv),
+        offsets=(i_dhqk * BLOCK_DQK, i_dhv * BLOCK_DV),
+        block_shape=(BLOCK_DQK, BLOCK_DV),
+        order=(1, 0),
+    )
+    matC_new_bptr = tl.make_block_ptr(
+        base=matC_new + i_bnh * s_matC_nh,
+        shape=(BLOCK_DQK, BLOCK_DV),
+        strides=(s_matC_dhqk, s_matC_dhv),
+        offsets=(i_dhqk * BLOCK_DQK, i_dhv * BLOCK_DV),
+        block_shape=(BLOCK_DQK, BLOCK_DV),
+        order=(1, 0),
+    )
+
+    vecN_old_ptr = (
+        vecN_old
+        + i_bnh * s_vecN_nh
+        + i_dhqk * BLOCK_DQK * s_vecN_dhqk
+        + tl.arange(0, BLOCK_DQK)
+    )
+    vecN_new_ptr = (
+        vecN_new
+        + i_bnh * s_vecN_nh
+        + i_dhqk * BLOCK_DQK * s_vecN_dhqk
+        + tl.arange(0, BLOCK_DQK)
+    )
+
+    scaM_old_ptr = scaM_old + i_bnh * s_scaM_nh
+    scaM_new_ptr = scaM_new + i_bnh * s_scaM_nh
+
+    vecQ_ptr = (
+        vecQ
+        + i_bnh * s_vecQK_nh
+        + i_dhqk * BLOCK_DQK * s_vecQK_dhqk
+        + tl.arange(0, BLOCK_DQK)
+    )
+    vecK_ptr = (
+        vecK
+        + i_bnh * s_vecQK_nh
+        + i_dhqk * BLOCK_DQK * s_vecQK_dhqk
+        + tl.arange(0, BLOCK_DQK)
+    )
+    vecV_ptr = (
+        vecV
+        + i_bnh * s_vecVH_nh
+        + i_dhv * BLOCK_DV * s_vecVH_dhv
+        + tl.arange(0, BLOCK_DV)
+    )
+
+    scaI_ptr = scaI + i_bnh * s_scaIF_nh
+    scaF_ptr = scaF + i_bnh * s_scaIF_nh
+
+
+    vecH_ptr = (
+        vecH
+        + i_bnh * s_vecVH_nh
+        + i_dhv * BLOCK_DV * s_vecVH_dhv
+        + tl.arange(0, BLOCK_DV)
+    )
+
+    #? Load data
+    # gates
+    scaF_val = tl.load(scaF_ptr)
+    scaI_val = tl.load(scaI_ptr)
+
+    scaFlog_val = tl.log(tl.sigmoid(scaF_val))
+
+    # update rule
+    scaM_new_val = tl.maximum(scaFlog_val + tl.load(scaM_old_ptr), scaI_val)
+
+    scaF_act = tl.exp(scaFlog_val + tl.load(scaM_old_ptr) - scaM_new_val)
+    scaI_act = tl.exp(scaI_val - scaM_new_val)
+
+    # TODO add masking to avoid out of bound access
+    vecK_val_scaled = tl.load(vecK_ptr) * qk_scale
+    vecV_val = tl.load(vecV_ptr)
+
+    matC_old_val = tl.load(matC_old_bptr, boundary_check=(0,1), padding_option="zero")
+
+    matC_new_val = (
+        scaF_act * matC_old_val + scaI_act * (vecK_val_scaled[:, None] * vecV_val[None, :])
+    )
+    
+    vecN_new_val = scaF_act * tl.load(vecN_old_ptr) + scaI_act * vecK_val_scaled
+
+    # outputs
+    vecQ_val = tl.load(vecQ_ptr) # TODO add masking to avoid out of bound access
+
+    h_num = vecQ_val[:, None] * matC_new_val
+    h_num = tl.sum(h_num, axis=0)
+    tl.static_print("vecQ_val", vecQ_val[:, None])
+    tl.static_print("h_num", h_num)
+
+    qn_dotproduct = tl.sum(vecQ_val * vecN_new_val)
+    tl.static_print("qn_dotproduct", qn_dotproduct)
+    max_val = tl.exp(-scaM_new_val)
+    tl.static_print("max_val", max_val)
+    h_denom = tl.maximum(tl.abs(qn_dotproduct), max_val) + EPS
+
+    h = tl.fdiv(h_num, h_denom)
+    tl.static_print("h", h)
+
+    tl.static_print("matC_new_val", matC_new_val)
+    tl.static_print("vecN_new_val", vecN_new_val)
+    
+    #? Store data
+    tl.store(matC_new_bptr, matC_new_val, boundary_check=(0,1))
+    tl.store(vecN_new_ptr, vecN_new_val) # TODO add masking to avoid out of bound access
+    tl.store(scaM_new_ptr, scaM_new_val)
+    tl.store(vecH_ptr, h)
 
 
 BLOCK_DQK = 16
@@ -62,7 +184,7 @@ BLOCK_DV = 16
 def recurrent_step_fw(
     matC_old: torch.Tensor,  # (B, NH, DHQK, DHV)
     vecN_old: torch.Tensor,  # (B, NH, DHQK)
-    scaM_old: torch.Tensor,  # (B, NH)
+    scaM_old: torch.Tensor,  # (B, NH, 1)
     vecQ: torch.Tensor,  # (B, NH, DHQK)
     vecK: torch.Tensor,  # (B, NH, DHQK)
     vecV: torch.Tensor,  # (B, NH, DHV)
@@ -71,6 +193,7 @@ def recurrent_step_fw(
     matC_new: torch.Tensor = None,  # (B, NH, DHQK, DHV)
     vecN_new: torch.Tensor = None,  # (B, NH, DHQK)
     scaM_new: torch.Tensor = None,  # (B, NH, 1)
+    qk_scale: float = None,
     DTYPE_GATE: torch.dtype = torch.float32,
     DTYPE_STATE: torch.dtype = torch.float32,
     DTPYE_QKV: torch.dtype = torch.float32,
@@ -79,6 +202,22 @@ def recurrent_step_fw(
     BLOCK_DV: int = 16,
 ):
     B, NH, DHQK, DHV = matC_old.shape
+
+
+    # cast inputs
+    matC_old = matC_old.to(DTYPE_STATE)
+    vecN_old = vecN_old.to(DTYPE_STATE)
+    scaM_old = scaM_old.to(DTYPE_STATE)
+
+    vecQ = vecQ.to(DTPYE_QKV)
+    vecK = vecK.to(DTPYE_QKV)
+    vecV = vecV.to(DTPYE_QKV)
+
+    scaI = scaI.to(DTYPE_GATE)
+    scaF = scaF.to(DTYPE_GATE)
+
+    if qk_scale is None:
+        qk_scale = 1 / math.sqrt(DHQK)
 
     if matC_new is None:
         assert (
@@ -114,17 +253,24 @@ def recurrent_step_fw(
         matC_new,
         vecN_new,
         scaM_new,
+        qk_scale,
         matC_old.stride(0),
         matC_old.stride(1),
         matC_old.stride(2),
+        matC_old.stride(3),
         vecN_old.stride(0),
         vecN_old.stride(1),
+        vecN_old.stride(2),
         scaM_old.stride(0),
         scaM_old.stride(1),
         vecQ.stride(0),
         vecQ.stride(1),
+        vecQ.stride(2),
         vecV.stride(0),
         vecV.stride(1),
+        vecV.stride(2),
+        scaI.stride(0),
+        scaI.stride(1),
         B,
         NH,
         DHQK,
