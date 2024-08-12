c3cdb0fd4d923d0b78b644c1f4fd140887c2627c
Maximilian Beck <beck@ml.jku.at>
2024-08-12 14:08:06 +0200

    make different kernels selectable.

--- run_mlstm_recurrent_step_fused_triton.py
+++ run_mlstm_recurrent_step_fused_triton.py
@@ -3,6 +3,8 @@ import sys
 sys.path.append("../..")
 import os
 
+import argparse
+
 os.environ["TRITON_PRINT_AUTOTUNING"] = "1"
 import torch
 
@@ -25,6 +27,26 @@ import triton
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--kernel", type=str, required=True, help="Kernel to benchmark."
+    )
+
+    args = parser.parse_args()
+
+    kernel = args.kernel
+    print(f"Kernel: {kernel}")
+    if kernel == "torch":
+        kernel_fn = recurrent_step_fw_torch
+    elif kernel == "torch_compile":
+        kernel_fn = torch.compile(recurrent_step_fw_torch)
+    elif kernel == "triton":
+        kernel_fn = recurrent_step_fw_triton
+    elif kernel == "triton_fused":
+        kernel_fn = recurrent_step_fw_triton_fused
+    else:
+        raise ValueError(f"Kernel {kernel} not recognized!")
+
     # params
     S = 12  # seq len
     B = 6  # batch size
@@ -46,31 +68,20 @@ if __name__ == "__main__":
     scaI = torch.randn((B, NH, 1), dtype=DTYPE, device=DEVICE)
     scaF = torch.randn((B, NH, 1), dtype=DTYPE, device=DEVICE)
 
-    BLOCK_DQK: int = 64
-    BLOCK_DV: int = 64
-
     warmup_iters = 5
     main_iters = 1000
 
     dt_state = torch.float16
 
+    if kernel == "torch" or kernel == "torch_compile":
+        inputs = (matC_old, vecN_old, scaM_old, vecQ, vecK, vecV, scaI, scaF)
+
+    else:
+        inputs = (matC_old, vecN_old, scaM_old, vecQ, vecK, vecV, scaI, scaF, dt_state)
+
     print(f"warmup")
     for i in tqdm(range(warmup_iters)):
-        h_out_pt, (matC_new_pt, vecN_new_pt, scaM_new_pt) = (
-            recurrent_step_fw_triton_fused(
-                matC_old,
-                vecN_old,
-                scaM_old,
-                vecQ,
-                vecK,
-                vecV,
-                scaI,
-                scaF,
-                DTYPE=dt_state,
-                # BLOCK_DQK=BLOCK_DQK,
-                # BLOCK_DV=BLOCK_DV,
-            )
-        )
+        h_out_pt, (matC_new_pt, vecN_new_pt, scaM_new_pt) = kernel_fn(*inputs)
 
     # print(f"main")
     # for i in tqdm(range(main_iters)):
