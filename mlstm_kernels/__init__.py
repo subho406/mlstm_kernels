b96fa6b823f9097a5639544d008e52c14899f917
Maximilian Beck <beck@ml.jku.at>
2024-08-13 07:37:51 +0200

    make kernel accessible via gloabl get_kernel function.

--- mlstm_kernels/__init__.py
+++ mlstm_kernels/__init__.py
@@ -9,7 +9,7 @@ def get_kernel(name: str) -> Callable:
     name = "<module_name>--<backend_name>"
 
     module_name: The name of the module containing the kernel function.
-        Example: "mlstm_chunkwise", "mlstm_parallel", "flash_attention", "flash_linear_attention"
+        Example: "mlstm_chunkwise", "mlstm_parallel", "mlstm_recurrent", "flash_attention", "flash_linear_attention"
 
     backend_name: The name of the kernel function as defined in the registry in the __init__.py file of the module.
     """
@@ -17,9 +17,11 @@ def get_kernel(name: str) -> Callable:
     from mlstm_kernels.baselines.flash_attention import registry as flash_attention_registry
     from mlstm_kernels.mlstm.chunkwise import registry as mlstm_chunkwise_registry
     from mlstm_kernels.mlstm.parallel import registry as mlstm_parallel_registry
+    from mlstm_kernels.mlstm.recurrent import registry_step as mlstm_recurrent_step_registry
     from mlstm_kernels.baselines.flash_linear_attention import registry as flash_linear_attention_gla_registry
 
     module_backend_registry = {
+        "mlstm_recurrent_step": mlstm_recurrent_step_registry,
         "mlstm_chunkwise": mlstm_chunkwise_registry,
         "mlstm_parallel": mlstm_parallel_registry,
         "flash_attention": flash_attention_registry,
--- mlstm_kernels/mlstm/recurrent/__init__.py
+++ mlstm_kernels/mlstm/recurrent/__init__.py
@@ -2,7 +2,7 @@ from .torch_fw import recurrent_step_fw
 from .triton_fw import recurrent_step_fw as recurrent_step_fw_triton
 from .triton_fused_fw import recurrent_step_fw as recurrent_step_fw_triton_fused
 
-registry = {
+registry_step = {
     "torch": recurrent_step_fw,
     "triton": recurrent_step_fw_triton,
     "triton_fused": recurrent_step_fw_triton_fused,
