272e37eb430c45034a02aa7a22daad173a4cf3d2
Maximilian Beck <beck@ml.jku.at>
2024-09-06 10:11:39 +0200

    fix: add *kwargs to chunkwise_torch_autograd.

--- mlstm_kernels/mlstm/chunkwise/torch_fwbw/torch_fwbw.py
+++ mlstm_kernels/mlstm/chunkwise/torch_fwbw/torch_fwbw.py
@@ -1,13 +1,13 @@
 # Copyright JKU Linz 2024
 # Author: Maximilian Beck
-import torch
 from typing import Callable
-from torch.amp import custom_fwd, custom_bwd
+
+import torch
+from torch.amp import custom_bwd, custom_fwd
 
 from ....kernel_utils import contiguous
-
-from ._torch_fw import _mlstm_chunkwise_fw
 from ._torch_bw import _mlstm_chunkwise_bw
+from ._torch_fw import _mlstm_chunkwise_fw
 
 """PyTorch.
 
@@ -206,9 +206,7 @@ def mlstm_chunkwise_fwbw(
     CHUNK_SIZE: int = 64,
     EPS: float = 1e-6,
     autocast_kernel_dtype: torch.dtype = torch.float16,
-) -> (
-    torch.Tensor | tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor, torch.Tensor]]
-):
+) -> torch.Tensor | tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor, torch.Tensor]]:
     _mlstm_chunkwise_fwbw = _get_chunkwise_fwbw_kernel(autocast_kernel_dtype)
     matH_out, matC_last, vecN_last, scaM_last = _mlstm_chunkwise_fwbw.apply(
         matQ,
@@ -243,9 +241,8 @@ def mlstm_chunkwise_torch_autograd(
     return_last_states: bool = False,
     eps: float = 1e-6,
     chunk_size: int = 64,
-) -> (
-    torch.Tensor | tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor, torch.Tensor]]
-):
+    **kwargs,
+) -> torch.Tensor | tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor, torch.Tensor]]:
     matH_out, _, _, last_states, _ = _mlstm_chunkwise_fw(
         matQ=q,
         matK=k,
@@ -279,9 +276,7 @@ def mlstm_chunkwise_torch_ownbw(
     eps: float = 1e-6,
     chunk_size: int = 64,
     autocast_kernel_dtype: torch.dtype = torch.float32,
-) -> (
-    torch.Tensor | tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor, torch.Tensor]]
-):
+) -> torch.Tensor | tuple[torch.Tensor, tuple[torch.Tensor, torch.Tensor, torch.Tensor]]:
     _mlstm_chunkwise_fwbw = _get_chunkwise_fwbw_kernel(autocast_kernel_dtype)
     matH_out, matC_last, vecN_last, scaM_last = _mlstm_chunkwise_fwbw.apply(
         q,
