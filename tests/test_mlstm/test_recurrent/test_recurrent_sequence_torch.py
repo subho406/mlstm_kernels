bd2dc0ea89de7014b5813a9e242ddec488fdba97
Maximilian Beck <beck@ml.jku.at>
2024-09-09 17:00:26 +0200

    add test for recurrent sequence.

---
+++ tests/test_mlstm/test_recurrent/test_recurrent_sequence_torch.py
@@ -0,0 +1,82 @@
+import pytest
+import logging
+import torch
+
+from mlstm_kernels.test_utils.test_templates.template_parallel_interface import (
+    template_test_parallel_interface,
+)
+from mlstm_kernels.test_utils.test_fixtures import test_session_folder  # noqa
+
+from mlstm_kernels.mlstm.parallel import (
+    mlstm_parallel_stable_torch_autograd,
+    mlstm_parallel_torch_autograd,
+)
+from mlstm_kernels.mlstm.recurrent import mlstm_recurrent_sequence_torch_autograd
+
+from ..test_params import final_combinations
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TestRecurrentSequenceTorchVsStableTorchLong:
+    @pytest.mark.skipif(not torch.cuda.is_available(), reason="No GPU available.")
+    @pytest.mark.xfail(reason="Fails due to numerical instability")
+    @pytest.mark.parametrize(
+        ["S", "B", "NH", "DHQK", "DHHV", "target_dtype"], final_combinations
+    )
+    def test_recurrent_sequence_torch_vs_stable_torch(
+        self, test_session_folder, S, B, NH, DHQK, DHHV, target_dtype
+    ):
+        print(f"S{S}B{B}NH{NH}DHQK{DHQK}DHHV{DHHV}")
+        template_test_parallel_interface(
+            baseline_fn=mlstm_parallel_stable_torch_autograd,
+            target_fn=mlstm_recurrent_sequence_torch_autograd,
+            baseline_name="torch_parallel_stable_ag",
+            target_name="torch_recurrent_sequence",
+            S=S,
+            B=B,
+            NH=NH,
+            DHQK=DHQK,
+            DHHV=DHHV,
+            dtype=getattr(torch, target_dtype),
+            atol_fw=1.0,  # 3.0
+            rtol_fw=1.0,
+            atol_fwbw=1.5,  # 3.5
+            rtol_fwbw=1.0,
+            vmax=1.0,
+            test_folder_name_prefix="recurrent_sequence-torch",
+            save_dir=str(test_session_folder),
+            add_fp64_baseline=True,
+        )
+
+
+class TestRecurrentSequenceTorchTritonVsUnstableTorchLong:
+    @pytest.mark.skipif(not torch.cuda.is_available(), reason="No GPU available.")
+    @pytest.mark.xfail(reason="Fails due to numerical instability")
+    @pytest.mark.parametrize(
+        ["S", "B", "NH", "DHQK", "DHHV", "target_dtype"], final_combinations
+    )
+    def test_recurrent_sequence_torch_vs_unstable_torch(
+        self, test_session_folder, S, B, NH, DHQK, DHHV, target_dtype
+    ):
+        print(f"S{S}B{B}NH{NH}DHQK{DHQK}DHHV{DHHV}")
+        template_test_parallel_interface(
+            baseline_fn=mlstm_parallel_torch_autograd,
+            target_fn=mlstm_recurrent_sequence_torch_autograd,
+            baseline_name="torch_parallel_unstable_ag",
+            target_name="torch_recurrent_sequence",
+            S=S,
+            B=B,
+            NH=NH,
+            DHQK=DHQK,
+            DHHV=DHHV,
+            dtype=getattr(torch, target_dtype),
+            atol_fw=1.0,  # 3.0
+            rtol_fw=1.0,
+            atol_fwbw=1.5,  # 3.5
+            rtol_fwbw=1.0,
+            vmax=1.0,
+            test_folder_name_prefix="recurrent_sequence",
+            save_dir=str(test_session_folder),
+            add_fp64_baseline=True,
+        )
