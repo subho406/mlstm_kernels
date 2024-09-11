9ef6bb93a561fc78fe1a7fa228684c3568142f68
Maximilian Beck <beck@ml.jku.at>
2024-09-11 14:21:04 +0200

    make chunkwise v3 usable from the outside.

--- mlstm_kernels/mlstm/chunkwise/__init__.py
+++ mlstm_kernels/mlstm/chunkwise/__init__.py
@@ -3,6 +3,7 @@ from .torch_fwbw import mlstm_chunkwise_torch_ownbw
 from .max_triton_fwbw import mlstm_chunkwise_max_triton
 from .max_triton_fwbw_v1 import mlstm_chunkwise_max_triton_v1
 from .max_triton_fwbw_v2 import mlstm_chunkwise_max_triton_v2
+from .max_triton_fwbw_v3 import mlstm_chunkwise_max_triton_v3
 from .triton_fwbw import mlstm_fwbw as mlstm_chunkwise_triton
 
 registry = {
@@ -11,5 +12,6 @@ registry = {
     "max_triton": mlstm_chunkwise_max_triton,  # fgate cumsum in dtype, intermediate states in dtype (kernel_dtype, e.g. float16, bfloat16)
     "max_triton_v1": mlstm_chunkwise_max_triton_v1,  # fgate cumsum in float32, intermediate states in float32
     "max_triton_v2": mlstm_chunkwise_max_triton_v2,  # fgate cumsum in float32, intermediate states in dtype (kernel_dtype, e.g. float16, bfloat16)
+    "max_triton_v3": mlstm_chunkwise_max_triton_v3,  # complete fgate grad in float32, intermediate states in float32
     "triton": mlstm_chunkwise_triton,  # TODO integrate newest version
 }
