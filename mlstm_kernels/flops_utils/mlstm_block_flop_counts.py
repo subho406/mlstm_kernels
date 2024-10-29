651d54734c771fa65d836767d998b94660ecac03
Maximilian Beck <maximilian.beck@nx-ai.com>
2024-10-29 19:22:58 +0100

    13 advance flop counting (#14)
    * add smaller chunk sizes to flop vs chunk size plot. Now we see the minimum.
    
    * feat: add model flop computation and transformer block flop computation.
    
    * update constants for flop calculation.
    
    * fix: factor in fw_C must be 4*L*dqk*dv.
    
    * fix: mlstmv1 block flops computation.
    
    * fix: linear layer flops for mlstm v2.
    
    * chore: reorganize flop computation of the mlstm.
    
    * feat: add slstm flop counts.
    
    * chore: compare slstm flops to mlstm flops
    
    * feat: integrate slstm block flop computation.
    
    ---------
    
    Co-authored-by: Maximilian Beck <beck@ml.jku.at>

--- mlstm_kernels/flops_utils/mlstm_block_flop_counts.py
+++ mlstm_kernels/flops_utils/mlstm_block_flop_counts.py
@@ -1,19 +1,259 @@
-"""Contains the final counts of the mlstm blocks."""
+"""Contains the functions to compute the FLOPs for the mlstm kernels and blocks."""
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+from collections.abc import Callable
+from dataclasses import dataclass
+from functools import partial
+from typing import Any, Literal
 
-from .flops_computation import FLOPsComputation, count_fw_flops
+
+def count_flops_fw_C(L, Nc, dqk, dv, Nh, factor_exp, factor_max, factor_mask) -> int:
+    """Flops for a single sequence."""
+    return (
+        Nc
+        * Nh
+        * (3 + 3 * L + factor_max + factor_exp * (1 + 2 * L) + 2 * dqk * dv + dqk + L * (4 * dqk * dv + 7 * dqk))
+    )
+
+
+def count_flops_fw_H(L, Nc, dqk, dv, Nh, factor_exp, factor_max, factor_mask) -> int:
+    """Flops for a single sequence."""
+    return (
+        Nc
+        * Nh
+        * (
+            (L * (L + 1)) // 2
+            + L * L * (7 + factor_mask + factor_max + factor_exp + 2 * dqk + 2 * dv)
+            + (1 + L) * factor_max
+        )
+    )
+
+
+def count_flops_mlstm_chunkwise_fw(L, Nc, dqk, dv, Nh, factor_exp, factor_max, factor_mask) -> tuple[int, int, int]:
+    flops_fw_C = count_flops_fw_C(L, Nc, dqk, dv, Nh, factor_exp, factor_max, factor_mask)
+    flops_fw_H = count_flops_fw_H(L, Nc, dqk, dv, Nh, factor_exp, factor_max, factor_mask)
+    flops_fw_total = flops_fw_C + flops_fw_H
+    return flops_fw_total, flops_fw_C, flops_fw_H
+
+
+# def _count_ln_flops(d):
+#     # not sure about this
+#     # d for mean, d to subtract mean, d for variance, d for division
+#     return 4 * d
+
+
+def _count_ln_flops(d):
+    # not sure about this
+    # d for mean, d to subtract mean, d for variance, d for division
+    return 0
+
+
+def count_flops_mlstm_v1_layer_fw(
+    S,
+    d,
+    dqk,
+    dv,
+    Nh,
+    chunk_size,
+    factor_sig=1,
+    factor_exp=1,
+    factor_max=1,
+    factor_mask=1,
+    count_ln_flops: Callable[[int], int] = _count_ln_flops,
+) -> int:
+    L = chunk_size
+    Nc = S // L
+    qkvif_flops = 2 * S * d * Nh * (2 * dqk + dv + 2)
+    ogate_flops = 2 * S * d * Nh * dv + S * Nh * dv * factor_sig
+    out_proj = 2 * S * d * Nh * dv
+    mlstm_cell_flops = count_flops_mlstm_chunkwise_fw(
+        L=L,
+        Nc=Nc,
+        dqk=dqk,
+        dv=dv,
+        Nh=Nh,
+        factor_exp=factor_exp,
+        factor_max=factor_max,
+        factor_mask=factor_mask,
+    )
+    ln_flops = count_ln_flops(d) + count_ln_flops(dv)
+    skip_flops = S * d
+    return qkvif_flops + out_proj + ogate_flops + mlstm_cell_flops[0] + ln_flops + skip_flops
+
+
+def count_flops_ffn_layer_fw(S, d, pf, factor_gelu=1, count_ln_flops: Callable[[int], int] = _count_ln_flops) -> int:
+    upproj_flops = 2 * S * d * d * pf + S * d * pf * factor_gelu
+    downproj_flops = 2 * S * d * d * pf
+    ln_flops = count_ln_flops(d)
+    skip_flops = S * d
+    return upproj_flops + downproj_flops + ln_flops + skip_flops
+
+
+def count_flops_mlstm_v1_block_fw(
+    S,
+    d,
+    dqk,
+    dv,
+    Nh,
+    chunk_size=64,
+    pf_ffn=4,
+    factor_sig=1,
+    factor_exp=1,
+    factor_gelu=1,
+    factor_max=1,
+    factor_mask=1,
+    count_ln_flops: Callable[[int], int] = _count_ln_flops,
+    return_detailed_flops=False,
+    **kwargs,
+):
+    mlstm_v1_layer_flops = count_flops_mlstm_v1_layer_fw(
+        S=S,
+        d=d,
+        dqk=dqk,
+        dv=dv,
+        Nh=Nh,
+        chunk_size=chunk_size,
+        factor_sig=factor_sig,
+        factor_exp=factor_exp,
+        factor_max=factor_max,
+        factor_mask=factor_mask,
+        count_ln_flops=count_ln_flops,
+    )
+    ffn_layer_flops = count_flops_ffn_layer_fw(
+        S=S, d=d, pf=pf_ffn, factor_gelu=factor_gelu, count_ln_flops=count_ln_flops
+    )
+    total_flops = mlstm_v1_layer_flops + ffn_layer_flops
+    if return_detailed_flops:
+        return total_flops, dict(mlstm_v1_layer_flops=mlstm_v1_layer_flops, ffn_layer_flops=ffn_layer_flops)
+    total_linear_layer_flops = ffn_layer_flops
+    total_mlstm_flops = mlstm_v1_layer_flops
+    return total_flops, total_linear_layer_flops, total_mlstm_flops
+
+
+def count_flops_mlstm_v2_block_fw(
+    S,
+    d,
+    dqk,
+    dv,
+    Nh,
+    qk_block_size=4,
+    qk_pf=1,
+    v_block_size=4,
+    v_pf=1,
+    conv1d_kernel_size=4,
+    pf=2,
+    chunk_size=64,
+    factor_exp=1,
+    factor_swish=1,
+    factor_max=1,
+    factor_mask=1,
+    count_ln_flops: Callable[[int], int] = _count_ln_flops,
+    return_detailed_flops=False,
+    **kwargs,
+):
+    linear_layer_flops = 6 * S * d * d * pf + S * d * pf * factor_swish
+    qkv_proj_flops = 2 * pf * d * (2 * S * qk_block_size * qk_pf + S * v_block_size * v_pf)
+    conv1d_flops = (
+        2 * conv1d_kernel_size * (S + conv1d_kernel_size - 1) * pf * d + S * pf * d + S * pf * d * factor_swish
+    )
+    skip_ln_mlstm_flops = 3 * S * d * pf * v_pf + count_ln_flops(d * pf * v_pf)
+    skip_ln_linear_layer_flops = S * d + count_ln_flops(d)
+
+    mlstm_cell_flops = count_flops_mlstm_chunkwise_fw(
+        L=chunk_size,
+        Nc=S // chunk_size,
+        dqk=dqk,
+        dv=dv,
+        Nh=Nh,
+        factor_exp=factor_exp,
+        factor_max=factor_max,
+        factor_mask=factor_mask,
+    )
+    total_flops = (
+        linear_layer_flops
+        + qkv_proj_flops
+        + conv1d_flops
+        + skip_ln_mlstm_flops
+        + skip_ln_linear_layer_flops
+        + mlstm_cell_flops[0]
+    )
+    total_linear_layer_flops = linear_layer_flops + skip_ln_linear_layer_flops
+    mlstm_cell_conv_flops = mlstm_cell_flops[0] + conv1d_flops + skip_ln_mlstm_flops
+    if return_detailed_flops:
+        return total_flops, dict(
+            linear_layer_flops=linear_layer_flops,
+            qkv_proj_flops=qkv_proj_flops,
+            conv1d_flops=conv1d_flops,
+            skip_ln_mlstm_flops=skip_ln_mlstm_flops,
+            skip_ln_linear_layer_flops=skip_ln_linear_layer_flops,
+            mlstm_cell_total_flops=mlstm_cell_flops[0],
+            mlstm_cell_fw_C_flops=mlstm_cell_flops[1],
+            mlstm_cell_fw_H_flops=mlstm_cell_flops[2],
+        )
+    return total_flops, total_linear_layer_flops, mlstm_cell_conv_flops
+
+
+@dataclass
+class FLOPsComputation:
+    # inputs
+    model_type: Literal["mlstm_v1", "mlstm_v2"]
+    model_config: dict[str, Any]
+    model_size_name: str | None = None
+    model_tag: str | None = None
+    batch_size: int = 1
+
+    # results
+    total_flops: int | None = None
+    linear_layer_flops: int | None = None
+    mlstm_other_flops: int | None = None
+
+
+def count_fw_flops(
+    flop_computations: FLOPsComputation | list[FLOPsComputation],
+    multiply_by_2: bool = False,
+) -> FLOPsComputation | list[FLOPsComputation]:
+    out = []
+    if isinstance(flop_computations, FLOPsComputation):
+        flop_computations = [flop_computations]
+    for flop_comp in flop_computations:
+        if flop_comp.model_type == "mlstm_v1":
+            total_flops, linear_layer_flops, mlstm_other_flops = count_flops_mlstm_v1_block_fw(**flop_comp.model_config)
+        elif flop_comp.model_type == "mlstm_v2":
+            total_flops, linear_layer_flops, mlstm_other_flops = count_flops_mlstm_v2_block_fw(**flop_comp.model_config)
+        else:
+            raise ValueError(f"Model type {flop_comp.model_type} not supported")
+
+        if multiply_by_2:
+            # we have to multiply by 2 for mlstm_v1 to account for the two layers in v1
+            total_flops *= 2
+            linear_layer_flops *= 2
+            mlstm_other_flops *= 2
+
+        flop_comp.total_flops = total_flops * flop_comp.batch_size
+        flop_comp.linear_layer_flops = linear_layer_flops * flop_comp.batch_size
+        flop_comp.mlstm_other_flops = mlstm_other_flops * flop_comp.batch_size
+        out.append(flop_comp)
+
+    if len(out) == 1:
+        return out[0]
+    return out
+
 
 ## constants
+# flop_factors = {
+#     "factor_sig": 1,
+#     "factor_swish": 1,
+#     "factor_exp": 1,
+#     "factor_gelu": 1,
+#     "factor_max": 1,
+#     "factor_mask": 1,
+# }
 flop_factors = {
-    "factor_sig": 1,
-    "factor_swish": 1,
-    "factor_exp": 1,
-    "factor_gelu": 1,
-    "factor_max": 1,
-    "factor_mask": 1,
+    "factor_sig": 0,
+    "factor_swish": 0,
+    "factor_exp": 0,
+    "factor_gelu": 0,
+    "factor_max": 0,
+    "factor_mask": 0,
 }
 # gpt3 table embedding dims
 embedding_dims = {
@@ -27,14 +267,14 @@ embedding_dims = {
 
 
 def get_mlstm_v1_fw_flops(
-    sequence_length: int, batch_size: int, chunk_size: int
+    sequence_length: int, chunk_size: int, batch_size: int = 1, **kwargs
 ) -> dict[str, FLOPsComputation]:
     # num heads mlstm v1
     num_heads_mlstm_v1 = {
-        "125M": 4,
-        "350M": 4,
-        "760M": 4,
-        "1.3B": 4,
+        "125M": 4,  # 12,  # 4,
+        "350M": 4,  # 16,  # 4,
+        "760M": 4,  # 16,  # 4,
+        "1.3B": 4,  # 16,  # 4,
         "2.7B": 4,
         "7B": 8,
     }
@@ -50,7 +290,7 @@ def get_mlstm_v1_fw_flops(
                 "S": sequence_length,
                 "d": embedding_dims[model_tag],
                 "Nh": num_heads_mlstm_v1[model_tag],
-                "dqk": embedding_dims[model_tag] // num_heads_mlstm_v1[model_tag],
+                "dqk": (embedding_dims[model_tag] // num_heads_mlstm_v1[model_tag]),
                 "dv": embedding_dims[model_tag] // num_heads_mlstm_v1[model_tag],
                 "chunk_size": chunk_size,
                 "pf": pf_ffn,
@@ -62,7 +302,7 @@ def get_mlstm_v1_fw_flops(
 
 
 def get_mlstm_v2_fw_flops(
-    sequence_length: int, batch_size: int, chunk_size: int
+    sequence_length: int, chunk_size: int, batch_size: int = 1, multiply_by_2: bool = True
 ) -> dict[str, FLOPsComputation]:
     # num heads mlstm v2
     num_heads_mlstm_v2 = {
@@ -90,8 +330,7 @@ def get_mlstm_v2_fw_flops(
                 "S": sequence_length,
                 "d": embedding_dims[model_tag],
                 "Nh": num_heads_mlstm_v2[model_tag],
-                "dqk": (embedding_dims[model_tag] * pf)
-                // num_heads_mlstm_v2[model_tag],
+                "dqk": (embedding_dims[model_tag] * pf) // num_heads_mlstm_v2[model_tag],
                 "dv": (embedding_dims[model_tag] * pf) // num_heads_mlstm_v2[model_tag],
                 "qk_block_size": qk_block_size,
                 "qk_pf": qk_pf,
@@ -103,242 +342,18 @@ def get_mlstm_v2_fw_flops(
                 **flop_factors,
             },
         )
-        mlstm_v2_flop_computations[model_tag] = count_fw_flops(flopcomp)
+        mlstm_v2_flop_computations[model_tag] = count_fw_flops(flopcomp, multiply_by_2=multiply_by_2)
     return mlstm_v2_flop_computations
 
 
-def get_mlstm_v1_fw_7B_flops(sequence_length: int, batch_size: int, chunk_size: int):
-    num_heads_mlstm_v1 = {
-        "7B_nh1": 1,
-        "7B_nh2": 2,
-        "7B_nh4": 4,
-        "7B_nh8": 8,
-        "7B_nh16": 16,
-    }
-    pf_ffn = 4
-    mlstm_v1_flop_computations = {}
-    for model_tag in num_heads_mlstm_v1.keys():
-        flopcomp = FLOPsComputation(
-            batch_size=batch_size,
-            model_type="mlstm_v1",
-            model_tag=model_tag,
-            model_config={
-                "S": sequence_length,
-                "d": embedding_dims["7B"],
-                "Nh": num_heads_mlstm_v1[model_tag],
-                "dqk": embedding_dims["7B"] // num_heads_mlstm_v1[model_tag],
-                "dv": embedding_dims["7B"] // num_heads_mlstm_v1[model_tag],
-                "chunk_size": chunk_size,
-                "pf": pf_ffn,
-                **flop_factors,
-            },
-        )
-        mlstm_v1_flop_computations[model_tag] = count_fw_flops(flopcomp)
-    return mlstm_v1_flop_computations
+def _get_mlstm_fw_flop_dict(
+    mlstm_func: Callable, sequence_length: int, chunk_size: int, multiply_by_2: bool = False
+) -> dict[str, tuple[int, int, int]]:
+    flop_res_dict = mlstm_func(sequence_length=sequence_length, chunk_size=chunk_size, multiply_by_2=multiply_by_2)
+
+    flop_count_dict = {k: (v.total_flops, v.linear_layer_flops, v.mlstm_other_flops) for k, v in flop_res_dict.items()}
+    return flop_count_dict
 
 
-def get_mlstm_v2_fw_7B_flops(sequence_length: int, batch_size: int, chunk_size: int):
-    num_heads_mlstm_v2 = {
-        "7B_nh1": 1,
-        "7B_nh2": 2,
-        "7B_nh4": 4,
-        "7B_nh8": 8,
-        "7B_nh16": 16,
-    }
-    qk_block_size = 4
-    qk_pf = 1
-    v_block_size = 4
-    v_pf = 1
-    conv1d_kernel_size = 4
-    pf = 2
-    mlstm_v2_flop_computations = {}
-    for model_tag in num_heads_mlstm_v2.keys():
-        flopcomp = FLOPsComputation(
-            batch_size=batch_size,
-            model_type="mlstm_v2",
-            model_tag=model_tag,
-            model_config={
-                "S": sequence_length,
-                "d": embedding_dims["7B"],
-                "Nh": num_heads_mlstm_v2[model_tag],
-                "dqk": (embedding_dims["7B"] * pf) // num_heads_mlstm_v2[model_tag],
-                "dv": (embedding_dims["7B"] * pf) // num_heads_mlstm_v2[model_tag],
-                "qk_block_size": qk_block_size,
-                "qk_pf": qk_pf,
-                "v_block_size": v_block_size,
-                "v_pf": v_pf,
-                "conv1d_kernel_size": conv1d_kernel_size,
-                "chunk_size": chunk_size,
-                "pf": pf,
-                **flop_factors,
-            },
-        )
-        mlstm_v2_flop_computations[model_tag] = count_fw_flops(flopcomp)
-    return mlstm_v2_flop_computations
-
-
-## plotting
-def get_flops_array_for_sizes(
-    model_size_keys: list[str],
-    flop_computation_dict: dict[str, FLOPsComputation],
-    flop_type: str = "total_other_flops",
-) -> list[int]:
-    flop_vals = []
-    for msk in model_size_keys:
-        if flop_type == "total_flops":
-            flop_vals.append(flop_computation_dict[msk].total_flops)
-        elif flop_type == "mlstm_other_flops":
-            flop_vals.append(flop_computation_dict[msk].mlstm_other_flops)
-        elif flop_type == "linear_layer_flops":
-            flop_vals.append(flop_computation_dict[msk].linear_layer_flops)
-    return flop_vals
-
-
-def plot_mlstm_v1_v2_flop_comparison(
-    sequence_length: int,
-    batch_size: int,
-    chunk_size: int,
-    model_size_keys: list[str],
-    plot_only_total_flops: bool = True,
-):
-    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-    mlstm_flops_v1 = get_mlstm_v1_fw_flops(
-        sequence_length=sequence_length, batch_size=batch_size, chunk_size=chunk_size
-    )
-    mlstm_flops_v2 = get_mlstm_v2_fw_flops(
-        sequence_length=sequence_length, batch_size=batch_size, chunk_size=chunk_size
-    )
-
-    total_flops_v1 = get_flops_array_for_sizes(
-        model_size_keys, flop_computation_dict=mlstm_flops_v1, flop_type="total_flops"
-    )
-    mlstm_other_flops_v1 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v1,
-        flop_type="mlstm_other_flops",
-    )
-    linear_layer_flops_v1 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v1,
-        flop_type="linear_layer_flops",
-    )
-
-    total_flops_v2 = get_flops_array_for_sizes(
-        model_size_keys, flop_computation_dict=mlstm_flops_v2, flop_type="total_flops"
-    )
-    mlstm_other_flops_v2 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v2,
-        flop_type="mlstm_other_flops",
-    )
-    linear_layer_flops_v2 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v2,
-        flop_type="linear_layer_flops",
-    )
-
-    x_vals = np.arange(len(model_size_keys))
-    x_tick_labels = model_size_keys
-
-    ax.grid(alpha=0.5)
-    v1_color = "tab:blue"
-    v2_color = "tab:red"
-    ax.plot(
-        x_vals, total_flops_v1, label="Total FLOPs mLSTMv1", marker="o", color=v1_color
-    )
-    ax.plot(
-        x_vals, total_flops_v2, label="Total FLOPs mLSTMv2", marker="o", color=v2_color
-    )
-    if not plot_only_total_flops:
-        ax.plot(
-            x_vals,
-            mlstm_other_flops_v1,
-            label="mLSTM FLOPs mLSTMv1",
-            linestyle="--",
-            color=v1_color,
-        )
-        ax.plot(
-            x_vals,
-            mlstm_other_flops_v2,
-            label="mLSTM FLOPs mLSTMv2",
-            linestyle="--",
-            color=v2_color,
-        )
-        ax.plot(
-            x_vals,
-            linear_layer_flops_v1,
-            label="Linear Layer FLOPs mLSTMv1",
-            linestyle="-.",
-            color=v1_color,
-        )
-
-        ax.plot(
-            x_vals,
-            linear_layer_flops_v2,
-            label="Linear Layer FLOPs mLSTMv2",
-            linestyle="-.",
-            color=v2_color,
-        )
-
-    ax.set_xticks(x_vals)
-    ax.set_xticklabels(x_tick_labels)
-    ax.set_yscale("log")
-
-    ax.set_xlabel("Model Size")
-    ax.set_ylabel("FLOPs")
-    ax.set_title("mLSTM v1 vs mLSTM v2 Block FLOPs Comparison")
-    ax.legend()
-    return fig
-
-
-def make_flop_table(
-    model_size_keys: list[str],
-    mlstm_flops_v1: dict[str, FLOPsComputation],
-    mlstm_flops_v2: dict[str, FLOPsComputation],
-):
-    total_flops_v1 = get_flops_array_for_sizes(
-        model_size_keys, flop_computation_dict=mlstm_flops_v1, flop_type="total_flops"
-    )
-    mlstm_other_flops_v1 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v1,
-        flop_type="mlstm_other_flops",
-    )
-    linear_layer_flops_v1 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v1,
-        flop_type="linear_layer_flops",
-    )
-    total_flops_v2 = get_flops_array_for_sizes(
-        model_size_keys, flop_computation_dict=mlstm_flops_v2, flop_type="total_flops"
-    )
-    mlstm_other_flops_v2 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v2,
-        flop_type="mlstm_other_flops",
-    )
-    linear_layer_flops_v2 = get_flops_array_for_sizes(
-        model_size_keys,
-        flop_computation_dict=mlstm_flops_v2,
-        flop_type="linear_layer_flops",
-    )
-
-    v2_v1_ratio = np.array(total_flops_v2) / np.array(total_flops_v1)
-
-    v1_linear_layer_ratio = np.array(linear_layer_flops_v1) / np.array(total_flops_v1)
-    v2_linear_layer_ratio = np.array(linear_layer_flops_v2) / np.array(total_flops_v2)
-
-    v1_mlstm_other_ratio = np.array(mlstm_other_flops_v1) / np.array(total_flops_v1)
-    v2_mlstm_other_ratio = np.array(mlstm_other_flops_v2) / np.array(total_flops_v2)
-
-    dat_dict = {
-        "Model Size": model_size_keys,
-        "Total FLOPs v1": total_flops_v1,
-        "Total FLOPs v2": total_flops_v2,
-        "v2/v1 Ratio": v2_v1_ratio,
-        "v1 Linear Layer FLOPs Ratio": v1_linear_layer_ratio,
-        "v2 Linear Layer FLOPs Ratio": v2_linear_layer_ratio,
-        "v1 MLSTM Other FLOPs Ratio": v1_mlstm_other_ratio,
-        "v2 MLSTM Other FLOPs Ratio": v2_mlstm_other_ratio,
-    }
-    return pd.DataFrame(dat_dict)
+get_mlstm_v1_fw_flop_dict = partial(_get_mlstm_fw_flop_dict, mlstm_func=get_mlstm_v1_fw_flops)
+get_mlstm_v2_fw_flop_dict = partial(_get_mlstm_fw_flop_dict, mlstm_func=get_mlstm_v2_fw_flops)
---
+++ mlstm_kernels/flops_utils/transformer_block_flop_counts.py
@@ -0,0 +1,35 @@
+def count_flops_transformer_block_fw(S: int, d: int, Nh: int, ff_ratio: float = 4.0) -> tuple[int, int, int]:
+    """DeepMind method for forwad pass FLOPs counting of decoder-only Transformer
+    See Chinchilla paper or this blog post:
+    https://www.adamcasson.com/posts/transformer-flops
+    """
+    d_attn = d // Nh
+    d_ff = d * ff_ratio
+
+    attn_qkv = 2 * S * 3 * d * (d_attn * Nh)
+    attn_logits = 2 * S * S * (d_attn * Nh)
+    attn_softmax = 3 * Nh * S * S
+    attn_reduce = 2 * S * S * (d_attn * Nh)
+    attn_project = 2 * S * (d_attn * Nh) * d
+    total_attn = attn_qkv + attn_logits + attn_softmax + attn_reduce + attn_project
+
+    ff = 2 * S * (d * d_ff + d * d_ff)
+
+    total_flops = total_attn + ff
+
+    return int(total_flops), int(ff), int(total_attn)
+
+
+def get_transformer_fw_flop_dict(sequence_length: int) -> dict[str, tuple[int, int, int]]:
+    """Returns a dictionary with the FLOPs of the transformer block for different hidden sizes."""
+    transformer_size_dict = {
+        "125M": dict(d=768, Nh=12),
+        "350M": dict(d=1024, Nh=16),
+        "760M": dict(d=1536, Nh=16),
+        "1.3B": dict(d=2048, Nh=16),
+        "2.7B": dict(d=2560, Nh=32),
+        "7B": dict(d=4096, Nh=32),
+    }
+
+    flops_dict = {k: count_flops_transformer_block_fw(sequence_length, **v) for k, v in transformer_size_dict.items()}
+    return flops_dict
