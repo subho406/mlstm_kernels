e6cb007f338bece4352022d36a5414d6499f2798
Maximilian Beck <maximilian.beck@nx-ai.com>
2024-11-12 17:35:28 +0000

    fix: use import of correct kernel.

--- mlstm_kernels/mlstm/parallel/__init__.py
+++ mlstm_kernels/mlstm/parallel/__init__.py
@@ -3,7 +3,7 @@ from .stable_torch_fwbw import (
     mlstm_parallel_torch_ownbw as mlstm_parallel_stable_torch_ownbw,
 )
 from .torch_fwbw import mlstm_parallel_torch_autograd, mlstm_parallel_torch_ownbw
-from .triton_fwbw.triton_fwbw import mlstm_parallel_triton
+from .triton_fwbw_v2.triton_fwbw import mlstm_parallel_triton
 
 registry = {
     "torch_autograd": mlstm_parallel_torch_autograd,
