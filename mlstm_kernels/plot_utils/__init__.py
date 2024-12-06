04de9fb89ea42ad9cc2efbcb5f89d1064df8f2ee
Maximilian Beck <beck@ml.jku.at>
2024-12-06 19:55:13 +0100

    remove other _utils modules.

--- mlstm_kernels/plot_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .diff_imshow import plot_numerical_diffs_per_batchhead, plot_numerical_diffs_single
-from .diff_lineplot import (
-    compute_errors_per_batchhead,
-    plot_error_statistics_over_time_per_batchhead,
-)
