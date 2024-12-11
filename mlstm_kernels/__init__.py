3d18a906e148edc5d44c19d61174b11ff6557a29
Maximilian Beck <beck@ml.jku.at>
2024-12-11 06:22:12 +0100

    update versions.

--- mlstm_kernels/__init__.py
+++ mlstm_kernels/__init__.py
@@ -1,4 +1,4 @@
 #  Copyright (c) NXAI GmbH.
 #  This software may be used and distributed according to the terms of the NXAI Community License Agreement.
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
