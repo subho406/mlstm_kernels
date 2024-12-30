82d0efcb627f86d9c498ce3bdb6477447bf7f714
Maximilian Beck <maximilian.beck@nx-ai.com>
2024-12-30 07:34:06 +0000

    feat: add siging kernels to registries

--- mlstm_kernels/jax/parallel/__init__.py
+++ mlstm_kernels/jax/parallel/__init__.py
@@ -2,6 +2,10 @@
 #  This software may be used and distributed according to the terms of the NXAI Community License Agreement.
 
 from .native import mlstm_parallel__native_autograd, mlstm_parallel__native_custbw
+from .native_siging import (
+    mlstm_siging_parallel__native_autograd,
+    mlstm_siging_parallel__native_custbw,
+)
 from .native_stablef import (
     mlstm_parallel__native_stablef_autograd,
     mlstm_parallel__native_stablef_custbw,
@@ -12,4 +16,6 @@ registry = {
     "native_custbw": mlstm_parallel__native_custbw,
     "native_stablef_autograd": mlstm_parallel__native_stablef_autograd,
     "native_stablef_custbw": mlstm_parallel__native_stablef_custbw,
+    "native_siging_autograd": mlstm_siging_parallel__native_autograd,
+    "native_siging_custbw": mlstm_siging_parallel__native_custbw,
 }
--- mlstm_kernels/torch/chunkwise/__init__.py
+++ mlstm_kernels/torch/chunkwise/__init__.py
@@ -4,10 +4,12 @@
 from .native import mlstm_chunkwise__native_autograd, mlstm_chunkwise__native_custbw
 from .triton_limit_chunk import mlstm_chunkwise__limit_chunk
 from .triton_xl_chunk import mlstm_chunkwise__xl_chunk
+from .triton_xl_chunk_siging import mlstm_siging_chunkwise__xl_chunk
 
 registry = {
     "native_autograd": mlstm_chunkwise__native_autograd,
     "native_custbw": mlstm_chunkwise__native_custbw,
     "triton_limit_chunk": mlstm_chunkwise__limit_chunk,
     "triton_xl_chunk": mlstm_chunkwise__xl_chunk,
+    "triton_xl_chunk_siging": mlstm_siging_chunkwise__xl_chunk,
 }
