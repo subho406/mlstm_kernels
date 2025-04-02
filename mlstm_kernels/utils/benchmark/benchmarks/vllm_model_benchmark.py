992dba0252277ec33a6ea2d2cb85e676598843e2
Korbinian Poeppel <korbinian.poeppel@nx-ai.com>
2025-04-02 12:12:45 +0000

    Remove unnecessary code for vllm benchmark.

--- mlstm_kernels/utils/benchmark/benchmarks/vllm_model_benchmark.py
+++ mlstm_kernels/utils/benchmark/benchmarks/vllm_model_benchmark.py
@@ -11,9 +11,6 @@ import torch
 import torch._dynamo.cache_size
 from vllm import LLM, SamplingParams
 
-from ..cuda_graphs import (
-    compile_with_cuda_graphs,
-)
 from ..param_handling import ModelSpec
 from .interface import BenchmarkFnContextManagerCfgType, ModelBenchmarkInterface
 
@@ -30,28 +27,6 @@ class VLLMModelBenchmark(ModelBenchmarkInterface):
     num_heads: int = 8
     num_blocks: int = 32
     vocab_size: int = 50304
-    # feedforward
-    ffn_proj_factor: float = 2.6667
-    ffn_round_up_to_multiple_of: int = 64
-    # mlstm
-    qk_dim_factor: float = 0.5
-    v_dim_factor: float = 1.0
-    # mlstm backend
-    chunkwise_kernel: str = "chunkwise--triton_xl_chunk"
-    sequence_kernel: str = "native_sequence__triton_step_fused"
-    step_kernel: str = "triton_fused"
-    mode: str = "inference"
-    chunk_size: int = 128
-
-    inference_state_dtype: str = "float32"
-    autocast_kernel_dtype: str = "bfloat16"
-
-    weight_mode: str = "fused"
-
-    # benchmark
-    amp_enabled: bool = True
-    amp_dtype: str = "bfloat16"
-    weight_dtype: str = "float32"
 
     use_torch_compile_model: bool = False # True
     use_torch_compile_generate: bool = False  # unused for now
@@ -113,24 +88,6 @@ class VLLMModelBenchmark(ModelBenchmarkInterface):
                 raise NotImplementedError(
                     f"Overrides for model {model_name} are not implemented."
                 )
-
-        if model_name == "xlstm":
-            model_config.vocab_size = self.vocab_size
-            model_config.embedding_dim = self.embedding_dim
-            model_config.num_heads = self.num_heads
-            model_config.num_blocks = self.num_blocks
-            model_config.ffn_proj_factor = self.ffn_proj_factor
-            model_config.ffn_round_up_to_multiple_of = self.ffn_round_up_to_multiple_of
-            model_config.qk_dim_factor = self.qk_dim_factor
-            model_config.v_dim_factor = self.v_dim_factor
-            model_config.chunkwise_kernel = self.chunkwise_kernel
-            model_config.sequence_kernel = self.sequence_kernel
-            model_config.step_kernel = self.step_kernel
-            model_config.chunk_size = self.chunk_size
-            model_config.mode = self.mode
-            model_config.inference_state_dtype = self.inference_state_dtype
-            model_config.autocast_kernel_dtype = self.autocast_kernel_dtype
-            model_config.weight_mode = self.weight_mode
 
         return model_config
 
@@ -280,11 +237,9 @@ class VLLMModelBenchmark(ModelBenchmarkInterface):
 def create_vllm_model_benchmark(
     model_spec: ModelSpec, param_dict: dict
 ) -> VLLMModelBenchmark:
-    from .model_benchmarks import mLSTMSimpleModelBenchmark
-
+    
     vllm_model_benchmark = VLLMModelBenchmark()
-    mlstm_simple_benchmark = mLSTMSimpleModelBenchmark()
-
+    
     model_name = model_spec.model_name
     if model_name in vllm_model_benchmark.available_kernels():
         vllm_model_benchmark.model_name = model_spec.model_name
@@ -296,18 +251,7 @@ def create_vllm_model_benchmark(
         vllm_model_benchmark.set_params(param_dict)
         return vllm_model_benchmark
 
-    elif model_name in mlstm_simple_benchmark.available_kernels():
-        mlstm_simple_benchmark.model_name = model_spec.model_name
-        mlstm_simple_benchmark.amp_enabled = False  # model_spec.amp_enabled
-        mlstm_simple_benchmark.amp_dtype = model_spec.amp_dtype
-        mlstm_simple_benchmark.weight_dtype = model_spec.weight_dtype
-        mlstm_simple_benchmark.use_torch_compile_model = (
-            model_spec.use_torch_compile_model
-        )
-
-        mlstm_simple_benchmark.set_params(param_dict)
-
-        return mlstm_simple_benchmark
+    
 
     else:
         raise ValueError(f"Unknown model name: {model_name}")
